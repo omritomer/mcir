164392f714cb840401a1441cc0039f19ca3d7e0b
omritomer <omritomer1@mail.tau.ac.il>
2019-11-25 15:53:34 +0200

    v0.0.1

---
+++ mcir/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Multi-component Inversion Recovery Analysis of Cortical Layers."""
+
+__author__ = """Omri Tomer"""
+__email__ = 'omritomer1@mail.tau.ac.il'
+__version__ = '0.0.1'
